88f86d423663b10a8865432f19092b275041222f
David Supplee <dwsupplee@gmail.com>
2013-11-29 11:31:02 -0500

    Update main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,6 @@
 
 angular.module('boostrapszApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
     $http.get('/api/images').success(function(images) {
       $scope.images = images;
     });
